c5042f5bc9dbecefedd589a9c123f0818b4601ca
Gorgant CX1 <gorgant@gorgant.com>
2019-06-29 21:45:55 +0800

    implement unsub email flow

--- functions/src/sendgrid/webhooks.ts
+++ functions/src/sendgrid/webhooks.ts
@@ -1,32 +1,36 @@
 import * as functions from 'firebase-functions';
 import { EmailEvent } from '../../../shared-models/email/email-event.model';
 import { updateEmailRecord } from './handlers';
-import { currentEnvironmentType } from '../environments/config';
-import { EnvironmentTypes } from '../../../shared-models/environments/env-vars.model';
+// import { currentEnvironmentType } from '../environments/config';
+// import { EnvironmentTypes } from '../../../shared-models/environments/env-vars.model';
 
-const isProductionEnv = (): boolean => {
-  switch (currentEnvironmentType) {
-    case EnvironmentTypes.PRODUCTION:
-      return true;
-    case EnvironmentTypes.SANDBOX:
-      return false;
-    default:
-      return false;
-  }
-}
+// const isProductionEnv = (): boolean => {
+//   switch (currentEnvironmentType) {
+//     case EnvironmentTypes.PRODUCTION:
+//       return true;
+//     case EnvironmentTypes.SANDBOX:
+//       return false;
+//     default:
+//       return false;
+//   }
+// }
+
+
+
+/////// DEPLOYABLE FUNCTIONS ///////
 
 // Receives an invoice payload from stripe
 export const sgEmailWebhookEndpoint = functions.https.onRequest(
 
   async (req, res) => {
 
-    // Prevents test data from using production webhook
-    // Sendgrid only allows one webhook, so be sure to switch Sendgrid webhook setting to the sandbox endpoint before commenting this out
-    if (!isProductionEnv()) {
-      console.log('Sandbox mode, canceling function, received this data', req.body);
-      res.sendStatus(200);
-      return;
-    }
+    // // Prevents test data from using production webhook
+    // // Sendgrid only allows one webhook, so be sure to switch Sendgrid webhook setting to the sandbox endpoint before commenting this out: https://app.sendgrid.com/settings/mail_settings
+    // if (!isProductionEnv()) {
+    //   console.log('Sandbox mode, canceling function, received this data', req.body);
+    //   res.sendStatus(200);
+    //   return;
+    // }
 
     try {
 
