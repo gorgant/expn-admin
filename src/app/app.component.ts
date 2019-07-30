d72449ffc41742016e8936592fd082fbd88dfc78
Gorgant CX1 <gorgant@gorgant.com>
2019-07-30 18:03:45 +0300

    upgrade to 1.7.7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { withLatestFrom } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit {
   title = 'Admin - Explearning';
-  appVersion = '1.7.6';
+  appVersion = '1.7.7';
 
   @ViewChild('sidenav', { static: true }) sidenav: MatSidenav;
 
