fc6b84061636bceb0c4dc795c8251509b13ea6d2
gorgant t15g <gorgant@gorgant.com>
2021-01-17 18:17:33 -0500

    upgrade to 2.0.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { ShorthandBusinessNames } from 'shared-models/forms-and-components/legal
 })
 export class AppComponent implements OnInit {
   title = `Admin - ${ShorthandBusinessNames.EXPN}`;
-  appVersion = '2.0.4';
+  appVersion = '2.0.5';
 
   @ViewChild('sidenav', { static: true }) sidenav: MatSidenav;
 
